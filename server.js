b72565adf414431d89eb8340d97d736ddade7e5b
Michell Platini <michell_platini21@hotmail.com>
2024-11-25 20:16:54 -0300

    Adicionando o projeto no GitHub

---
+++ server.js
@@ -0,0 +1,14 @@
+import app from './src/app.js'
+
+
+const porta = process.env.PORT || 3000;
+
+
+
+app.listen(porta,() => {
+    // http://localhost:3000/
+    console.log(`Servidor executando em http://localhost:${porta}/`);
+})
+// app.listen(porta, '0.0.0.0', () => {
+//   console.log(`Servidor escutando em http://0.0.0.0:${porta}/`);
+// });
---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import fs from 'fs';
+import cors from 'cors';
+
+import produtosRouter from './routes/produtosRoutes.js';
+
+import rotinaEstoque from './routes/rotinaVerificacaoEstoque.js';
+
+import fluxoRoutes from './routes/fluxoRoutes.js';
+
+/** Add arquivo de index.js */
+const html = fs.readFileSync('./src/index/index.html', 'utf8');
+
+const app = express();
+
+/** add o middleware Json */
+app.use(express.json());
+
+/** habilita CORS */
+app.use(cors({
+  origin: '*',
+}));
+
+app.get('/', (req, res) => {
+  res.status(200).send(html);
+});
+
+/** Add Rotas */
+app.use(produtosRouter);
+app.use(rotinaEstoque);
+app.use(fluxoRoutes);
+
+export default app;
